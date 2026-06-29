d4ab6649bdcd3f8ba9062f00ac9bb3c0e7fe1056
Ankit <ankitprajapati3785@gmail.com>
2026-06-29 19:02:13 +0530

    Fix URL paths in index.html for certificate and project images

--- fix.cjs
+++ fix.cjs
@@ -1,12 +1,13 @@
 const fs = require('fs');
 let html = fs.readFileSync('index.html', 'utf8');
 
-html = html.replace(/src=\"\/certificate-images\//g, 'src=\"./certificate-images/');
-html = html.replace(/href=\"\/certificate-images\//g, 'href=\"./certificate-images/');
-html = html.replace(/openCertificate\('\/certificates\//g, 'openCertificate(\'./certificates/');
-html = html.replace(/src=\"\/project-images\//g, 'src=\"./project-images/');
-html = html.replace(/href=\"\.\/public\/AjayVarshney_Resume.pdf\"/g, 'href=\"./AjayVarshney_Resume.pdf\"');
-html = html.replace(/src=\"\.\/public\/profile.jpg\"/g, 'src=\"./profile.jpg\"');
+html = html.replace(/src=\"\.\/certificate-images\//g, 'src=\"/certificate-images/');
+html = html.replace(/href=\"\.\/certificate-images\//g, 'href=\"/certificate-images/');
+html = html.replace(/openCertificate\(\'\.\/certificates\//g, 'openCertificate(\'/certificates/');
+html = html.replace(/src=\"\.\/project-images\//g, 'src=\"/project-images/');
+html = html.replace(/href=\"\.\/AnkitGola_Resume.pdf\"/g, 'href=\"/AnkitGola_Resume.pdf\"');
+html = html.replace(/src=\"\.\/profile.jpg\"/g, 'src=\"/profile.jpg\"');
+
 
 fs.writeFileSync('index.html', html);
 console.log('Fixed URLs in index.html');
